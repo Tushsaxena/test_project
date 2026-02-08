b5b8ef4acfe7670817bb9a657a45f5e441317b9c
TushSaxena <saxena.tushar98@gmail.com>
2026-02-08 18:23:45 +0530

    Commiting changes

--- rspec/spec/test_spec.rb
+++ rspec/spec/test_spec.rb
@@ -1,14 +1,91 @@
 require "selenium-webdriver"
 require "rspec"
 
-RSpec.describe "Google seach" do
+RSpec.describe "Testing framework" do
 
-  it "Open" do
+  before(:all) do
+    @driver = Selenium::WebDriver.for :chrome
 
-    driver = Selenium::WebDriver.for :chrome
-    driver.navigate.to "https://www.google.com"
-
-    expect(driver.title).to include("Google")
-    driver.quit
   end
+
+  after(:all)do
+    @driver.quit
+  end
+
+  context 'testing elements' do
+
+    it "Open demo qa" do
+      @driver.navigate.to "https://demoqa.com/"
+
+      wait = Selenium::WebDriver::Wait.new(timeout: 10)
+
+      element = wait.until {
+        @driver.find_element(xpath: "//h5[text()='Elements']")
+      }
+
+      # SCROLL FIRST
+      @driver.execute_script(
+        "arguments[0].scrollIntoView({block: 'center'});",
+        element
+      )
+
+      # NOW check visibility
+      puts element.displayed? ? "Element is visible" : "Element is NOT visible"
+
+      # WAIT until clickable (important)
+      wait.until { element.displayed? && element.enabled? }
+
+      element.click
+
+
+    end
+
+    it 'Fetch content of table' do
+      element = @driver.find_element(xpath: "//*[@class='left-pannel']")
+      puts element.displayed? ? "Element is visible" : "Element is NOT visible"
+
+      puts "---element text---"
+      puts element.text
+
+      arr = element.text.split("\n")   # split by line
+      puts "---element in array---"
+      arr.pop
+      puts arr.inspect
+
+      expect(arr).to eq(["Elements", " Text Box", "Check Box", "Radio Button", "Web Tables", "Buttons", "Links", "Broken Links - Images", "Upload and Download", "Dynamic Properties", "Forms", " Alerts, Frame & Windows", " Widgets", " Interactions", " Book Store Application"])
+    end
+
+    it 'Fill the details' do
+      button = @driver.find_element(xpath: "//span[text()='Text Box']")
+      button.click
+
+      text = @driver.find_element(id: "userName")
+      text.send_keys("Tushar Saxena")
+
+      email = @driver.find_element(id: "userEmail")
+      email.send_keys("testqa98@gmail.com")
+
+      submit = @driver.find_element(id: "submit")
+
+      @driver.execute_script(
+        "arguments[0].scrollIntoView({block: 'center'});", submit)
+
+      submit.click
+      puts"-----------------------------"
+      content_output=@driver.find_element(id: "output")
+      content_output= content_output.text
+
+      lines=content_output.lines
+      pairs= lines.map do |line|
+        parts=line.split(":")
+        parts.map {|p| p.strip}
+      end
+      content_output=pairs.to_h
+      puts"--content_output----#{content_output}"
+      expect(content_output["Name"]).to eq("Tushar Saxena")
+      expect(content_output["Email"]).to eq("testqa98@gmail.com")
+    end
+
+  end
+
 end
